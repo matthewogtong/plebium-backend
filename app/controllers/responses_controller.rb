599d4f5074d02e5a8ea89ecd911709ccf77f890e
Sara Cemal <you@example.com>
2021-01-25 23:27:57 -0500

    added new routes/controllers for stories and responses

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,17 @@
 class ResponsesController < ApplicationController
+    def index
+        @responses = Response.all
+        render json: @responses
+    end
+
+    def create
+        @response= Response.create(response_params)
+        render json: @response
+    end
+
+    private
+
+    def response_params
+        params.permit(:content, :snaps, :story_id)
+    end
 end
